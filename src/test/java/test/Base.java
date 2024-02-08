c52330d536a6654b6b788e62a0d0757f1d1019d0
VORVALK <vorvalk@outlook.com>
2024-02-08 17:09:49 +0000

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,7 +30,7 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.random()+71;
+        double r = Math.random()-71;
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
